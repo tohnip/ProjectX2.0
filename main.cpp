e08e65ca363a95b1e94aff74771f919a4f590042
Patrik Tohni <patrik.tohni@student.oulu.fi>
2024-09-22 16:48:30 +0300

    Replaced placeholders with files from earlier tasks

--- main.cpp
+++ main.cpp
@@ -1,8 +1,67 @@
+#include <assert.h>
+#include <functional>
+#include <map>
+#include <iostream>
+#include <string>
+#include <iterator>
+
 #include "src/calculator.h"
 
-int main() {
+int main()
+{
     // Function calls to calculate
     // Write your all possible input scenarios here
     // Notice that calculator code written here works with integers
+
+    std::map<char, std::function<float(float, float)>> operations;
+    operations['+'] = [](float v1, float v2) { return v1+v2; };
+    operations['-'] = [](float v1, float v2) { return v1-v2; };
+    operations['*'] = [](float v1, float v2) { return v1*v2; };
+    operations['/'] = [](float v1, float v2) { return v1/v2; };
+
+
+    for (std::map<char, std::function<float(float, v)>>::iterator it = operations.begin() ; it != operations.end() ; ++it)
+    {
+        char op = it->first;
+        std::function<float(float,float)> operation = it->second;
+
+        for (int value1 = -123; value1 < 123; value1++)
+        {
+            for (int value2 = -123; value2 < 123; value2++)
+            {
+                float result;
+                if (op == '/' && value2 == 0)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = operation(value1, value2);
+                }
+
+                std::cout << std::to_string(value1) + op + std::to_string(value2) + " = " + std::to_string(result) << std::endl;
+                assert(calculator(value1, op, value2) == result);
+            }
+        }
+    }
+
+    for (uint i = 0; i < 256; i++)
+    {
+        int value1 = -999;
+        int value2 = 12345;
+        char op = static_cast<char>(i);
+
+        if (operations.find(op) == operations.end())
+        {
+            assert(calculator(value1, op, value2) == 1);
+        }
+        else
+        {
+            float result = operations[op](value1, value2);
+            std::cout << std::to_string(value1) + op + std::to_string(value2) + " = " + std::to_string(result) << std::endl;
+            assert(calculator(value1, op, value2) == result);
+        }
+    }
+
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,31 +1,30 @@
 #include <assert.h>
-#include "../src/calculator.h"
+#include <set>
+
 #include <cifuzz/cifuzz.h>
 #include <fuzzer/FuzzedDataProvider.h>
-#include <cstdint>
-#include <cstddef>
-#include <string>
+
+#include "../src/calculator.h"
 
 FUZZ_TEST_SETUP() {
   // Perform any one-time setup required by the FUZZ_TEST function.
 }
 
-FUZZ_TEST(const uint8_t *data, size_t size) {
-  // FuzzedDataProvider provides convenience methods that turn the raw fuzzer
-  // data into common types. Use it to generate parameters for the function you
-  // want to fuzz:
-  //
-  // FuzzedDataProvider fuzzed_data(data, size);
-  // int my_int = fuzzed_data.ConsumeIntegral<int8_t>();
-  // std::string my_string = fuzzed_data.ConsumeRandomLengthString();
+FUZZ_TEST(const uint8_t *data, size_t size)
+{
+  FuzzedDataProvider fuzzed_data(data, size);
+  float value1 = fuzzed_data.ConsumeIntegral<float>();
+  float value2 = fuzzed_data.ConsumeIntegral<float>();
 
-  // Call the functions you want to test with the provided data and optionally
-  // assert that the results are as expected:
-  //
-  // int res = DoSomething(my_int, my_string);
-  // assert(res != -1);
+  std::string random_string = fuzzed_data.ConsumeRandomLengthString();
+  char op = random_string[0];
 
-  // If you want to know more about writing fuzz tests you can check out the
-  // example projects at https://github.com/CodeIntelligenceTesting/cifuzz/tree/main/examples
-  // or have a look at our docs at https://docs.code-intelligence.com/
+  float res = calculator(value1, op, value2);
+
+  std::set<char> valid_ops({'+', '-', '*', '/'});
+
+  if (valid_ops.find(op) == valid_ops.end())
+  {
+    assert(res == 1);
+  }
 }
